e1e1f3d781127644a0d9a4b083ca3785884a2ce6
NATALIA RIOS <natalya.rios@gmail.com>
2026-03-04 22:08:22 -0300

    feat(terraform): adiciona configuração de infraestrutura

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.4.0"
+    }
+  }
+}
+
+provider "local" {}
+
+resource "local_file" "infra_demo" {
+  filename = "infra_terraform.txt"
+  content  = "Infraestrutura criada automaticamente com Terraform."
+}
